67fdef2f0abce3cfb564d38e28d84e75d1e96702
run3wide <paulvrobson@gmail.com>
2021-07-28 23:34:46 -0400

    Add linting

--- src/jvmMain/kotlin/server.kt
+++ src/jvmMain/kotlin/server.kt
@@ -1,12 +1,12 @@
 import io.ktor.application.call
 import io.ktor.html.respondHtml
 import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import io.ktor.http.content.resources
-import io.ktor.http.content.static
 import kotlinx.html.*
 
 fun HTML.index() {
