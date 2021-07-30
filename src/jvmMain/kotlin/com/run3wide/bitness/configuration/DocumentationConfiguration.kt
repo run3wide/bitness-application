186550e0816bb450a0e2031fec3e46365817917a
run3wide <paulvrobson@gmail.com>
2021-07-29 20:31:11 -0400

    Add pricing docs

---
+++ src/jvmMain/kotlin/com/run3wide/bitness/configuration/DocumentationConfiguration.kt
@@ -0,0 +1,29 @@
+package com.run3wide.bitness.configuration
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.info.License
+import io.swagger.v3.oas.annotations.servers.Server
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "Bitness API",
+        description = "REST API serving cryptocurrency exchange rates.",
+        contact = Contact(
+            name = "run3wide",
+            url = "https://run3wide.com",
+            email = "paul@run3wide.com",
+        ),
+        license = License(
+            name = "Apache 2.0",
+            url = "https://www.apache.org/licenses/LICENSE-2.0.html",
+        )
+    ),
+    servers = [
+        Server(
+            url = "bitness-api.run3wide.com",
+        )
+    ],
+)
+class DocumentationConfiguration
--- src/jvmMain/kotlin/com/run3wide/bitness/pricing/PricingController.kt
+++ src/jvmMain/kotlin/com/run3wide/bitness/pricing/PricingController.kt
@@ -1,12 +1,17 @@
 package com.run3wide.bitness.pricing
 
 import dto.pricing.PriceDto
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import io.swagger.v3.oas.annotations.tags.Tags
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tags(Tag(name = "Pricing"))
 @RestController
 class PricingController {
 
+    @Operation(summary = "Get fake prices")
     @GetMapping("/rest/prices")
     fun getBitcoinPrice(): List<PriceDto> {
         return listOf(
