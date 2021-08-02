2e5e80a04f3af2d288c8c10a92e3cf6458a3e6bc
Paul Robson <paulvrobson@gmail.com>
2021-08-02 16:52:03 -0400

    Format exchange rates to 2 decimal places

--- src/jsMain/kotlin/component/Router.kt
+++ src/jsMain/kotlin/component/Router.kt
@@ -1,15 +1,21 @@
 package component
 
+import kotlinx.css.pct
+import kotlinx.css.width
 import react.RProps
 import react.child
 import react.fc
 import react.router.dom.browserRouter
 import react.router.dom.route
 import react.router.dom.switch
+import styled.css
 import styled.styledDiv
 
 val AppRouter = fc<RProps> {
     styledDiv {
+        css {
+            width = 100.pct
+        }
         browserRouter {
             child(NavBar)
             switch {
--- src/jsMain/kotlin/component/WelcomePage.kt
+++ src/jsMain/kotlin/component/WelcomePage.kt
@@ -1,6 +1,8 @@
 package component
 
+import kotlinx.css.Display
 import kotlinx.css.JustifyContent
+import kotlinx.css.display
 import kotlinx.css.justifyContent
 import react.RProps
 import react.fc
@@ -10,6 +12,7 @@ import styled.styledDiv
 val WelcomePage = fc<RProps> {
     styledDiv {
         css {
+            display = Display.flex
             justifyContent = JustifyContent.center
         }
         +"Welcome to Bitness!"
