86a1dcbec017f8a41438cb2099763ada882e6a42
Paul Robson <paulvrobson@gmail.com>
2021-08-02 21:52:08 -0400

    Extract helper methods for ExchangeRate formatting

--- src/jsMain/kotlin/component/Ticker.kt
+++ src/jsMain/kotlin/component/Ticker.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.launch
 import kotlinx.css.*
 import org.w3c.dom.EventSource
 import org.w3c.dom.MessageEvent
+import org.w3c.dom.events.Event
 import react.*
 import react.dom.attrs
 import react.dom.img
@@ -22,20 +23,18 @@ val Ticker = fc<TickerProps> { props ->
 
     useEffect {
         GlobalScope.launch {
-            setExchangeRate(
-                BitnessApi.getCurrentExchangeRateInUsd(props.currencySymbol).format()
-            )
+            val formattedExchangeRate = BitnessApi
+                .getCurrentExchangeRateInUsd(props.currencySymbol)
+                .format()
+            setExchangeRate(formattedExchangeRate)
         }
-        eventSource.addEventListener(
-            "${props.currencySymbol}_PRICE",
-            {
-                val data = (it as MessageEvent).data.toString().toDouble()
-                setExchangeRate(data.format())
-            },
-        )
-        this.cleanup {
-            eventSource.close()
+        val callback: (Event) -> Unit = {
+            val formattedExchangeRate = it.toExchangeRate().format()
+            setExchangeRate(formattedExchangeRate)
         }
+        val eventName = "${props.currencySymbol}_PRICE"
+        eventSource.addEventListener(eventName, callback)
+        this.cleanup { eventSource.close() }
     }
 
     exchangeRate?.let { exchangeRateInUsd ->
@@ -57,3 +56,8 @@ val Ticker = fc<TickerProps> { props ->
 }
 
 private fun Double.format(): String = this.asDynamic().toFixed(2) as String
+
+private fun Event.toExchangeRate() = (this as MessageEvent)
+    .data
+    .toString()
+    .toDouble()
